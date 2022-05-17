e07cccfd9a02fa93766bb4cf0fc3ec3b2980c577
YamiKawa1 <jehanzao@gmail.com>
2022-05-17 18:43:05 -0400

    add abilities

--- src/components/abilities/Abilities.jsx
+++ src/components/abilities/Abilities.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './Abilities.css'
+import {data} from './AbilitiesData'
+
+const makeAbilities = data.map(ability => {
+    return (
+      <div className='ability'>
+        <img src={ability.img} alt="" />
+        <small>{ability.name}</small>
+      </div>
+    )
+  })
+
 
 function Abilities() {
   return (
-    <div className='abilities'>
-        <div className='ability'>
-Nodejs
-        </div>
+    <div className='main-abilities'>
+      <h4>Habilidades, herramientas y conocimientos</h4>
+      <div className='abilities'>
+        {makeAbilities}
+      </div>
     </div>
+    
   )
 }
 
